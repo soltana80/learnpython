192dfb709c0ae4d1c83749d22ce9bd8c9cf053e6
soltana80 <flodre80@googlemail.com>
2019-05-21 17:36:39 +0200

    opencv erste versuche mit screenshot uber pyscreenshot

---
+++ opencv/screenshot.py
@@ -0,0 +1,7 @@
+import numpy
+import cv2
+import pyscreenshot as ImageGrab
+
+im = ImageGrab.grab(bbox=(10,10,510,510))
+
+im.show()
---
+++ opencv/testopencv.py
@@ -0,0 +1,6 @@
+import cv2
+import sys
+
+print(sys.version_info)
+print("Platform:" + sys.platform)
+print("openCV Versionsnummern:" + cv2.__version__[0])
